470d1df0d2b86fdb7d7553454bd72f5742b857e3
Kevin Mendes <papawa.dev@gmail.com>
2024-06-25 23:50:40 +0200

    - needed 6 vertices for 2 triangles

--- src/mesh.rs
+++ src/mesh.rs
@@ -63,7 +63,7 @@ impl<T> Mesh<T> {
 }
 
 //DEFAULT
-const VERTICES: [Vertex; 4] = [
+const VERTICES: [Vertex; 6] = [
     Vertex::new(
         glam::vec3(1.0, 1.0, 0.0),
         glam::vec3(1.0, 0.0, 0.0),
@@ -80,8 +80,18 @@ const VERTICES: [Vertex; 4] = [
         glam::Vec3::ZERO,
     ),
     Vertex::new(
-        glam::vec3(3.0, -1.5, 2.5),
-        glam::vec3(0.5, 0.5, 0.5),
+        glam::vec3(1.0, 1.0, 0.0),
+        glam::vec3(1.0, 0.0, 0.0),
+        glam::Vec3::ZERO,
+    ),
+    Vertex::new(
+        glam::vec3(-1.0, 1.0, 0.0),
+        glam::vec3(0.0, 1.0, 0.0),
+        glam::Vec3::ZERO,
+    ),
+    Vertex::new(
+        glam::vec3(0.0, 3.0, 0.0),
+        glam::vec3(0.0, 0.0, 1.0),
         glam::Vec3::ZERO,
     ),
 ];
