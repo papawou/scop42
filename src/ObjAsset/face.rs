65684b7d1e1ce3c646efeeba3a1b3fa76b9c2725
Kevin Mendes <papawa.dev@gmail.com>
2024-08-25 01:10:07 +0200

    ObjAsset(VertexAttribute.parse): disallow '/' at the end of VertexAttribute part

--- src/ObjAsset/face.rs
+++ src/ObjAsset/face.rs
@@ -10,7 +10,6 @@ struct VertexAttribute {
 
 impl VertexAttribute {
     fn vertex_attribute_parse(str: &str) -> Option<u32> {
-        println!("{}", str);
         if str.is_empty() {
             None
         } else {
@@ -19,15 +18,24 @@ impl VertexAttribute {
     }
 
     pub fn parse(str: &str) -> Self {
-        let mut data = str.split('/');
-
-        let vertex_index = data.next().and_then(Self::vertex_attribute_parse).unwrap();
-        let vertex_texture_index = data.next().and_then(Self::vertex_attribute_parse);
-        let vertex_normal_index = data.next().and_then(Self::vertex_attribute_parse);
-
-        if (data.next().is_some()) {
-            panic!();
+        {
+            let test: Vec<&str> = str.split('/').collect();
+            dbg!(test);
         }
+
+        let mut raw_vertex_attributes: Vec<Option<u32>> =
+            str.split('/').map(Self::vertex_attribute_parse).collect();
+
+        if raw_vertex_attributes.len() > 3 {
+            panic!("{:?}", &raw_vertex_attributes);
+        }
+
+        raw_vertex_attributes.resize(3, None);
+        let (vertex_index, vertex_texture_index, vertex_normal_index) =
+            match raw_vertex_attributes[..] {
+                [Some(v_index), tex_index, norm_index] => (v_index, tex_index, norm_index),
+                _ => panic!("{:?}", raw_vertex_attributes),
+            };
 
         Self {
             vertex_index,
