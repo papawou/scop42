3c65eb191061f6652122f2b44bf0663fa564ce82
papawou <papawa.gaming@gmail.com>
2024-09-17 01:25:41 +0200

    added Makefile: compile-shaders option

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::process::{Command, Stdio};
+
+fn main() {
+    // println!("cargo:rerun-if-changed=build.rs");
+
+    // let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    // let status = Command::new("cmd")
+    //     .arg("/C")
+    //     .arg(format!(
+    //         "docker run --rm -i -v {}/shaders:/host shaderc",
+    //         cargo_manifest_dir
+    //     ))
+    //     .stdin(Stdio::inherit())
+    //     .stdout(Stdio::inherit())
+    //     .stderr(Stdio::inherit())
+    //     .status()
+    //     .unwrap();
+
+    // if !status.success() {
+    //     panic!("Docker  command failed with exit code {:?}", status.code());
+    // }
+}
