f15522ce32651b099a7a999a468e3b024ad520da
Kevin Mendes <kmendes@c3r4s2.42lausanne.ch>
2024-09-16 19:04:38 +0200

    glslc

---
+++ shaders/compile_shaders.sh
@@ -0,0 +1,10 @@
+glslc -g -o ./tri.frag.spv ./tri.frag
+glslc -g -o ./tri.vert.spv ./tri.vert
+
+glslc -g -o ./colored_tri.frag.spv ./colored_tri.frag
+glslc -g -o ./colored_tri.vert.spv ./colored_tri.vert
+
+glslc -g -o ./mesh.vert.spv ./mesh.vert
+glslc -g -o ./mesh.frag.spv ./mesh.frag
+
+glslc -g -o ./mesh_dba.vert.spv ./mesh_dba.vert
