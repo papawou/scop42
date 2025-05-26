0d3b05dd07d86e568201ceccb10827f5322991da
Kevin Mendes <papawa.dev@gmail.com>
2025-05-26 20:00:24 +0200

    wip

--- ecs/src/query.rs
+++ ecs/src/query.rs
@@ -4,14 +4,14 @@ use crate::{component::Component, entity::Entity, storage::ComponentsStorage};
 
 pub struct Query<'a, Q>
 where
-    Q: Fetch,
+    Q: Fetch<'a>,
 {
     world: &'a ComponentsStorage,
     _marker: PhantomData<Q>,
 }
 impl<'a, Q> Query<'a, Q>
 where
-    Q: Fetch,
+    Q: Fetch<'a>,
 {
     pub fn new(world: &'a ComponentsStorage) -> Self {
         Self {
@@ -21,9 +21,9 @@ where
     }
 }
 
-impl<Q> IntoIterator for Query<'_, Q>
+impl<'a, Q> IntoIterator for Query<'a, Q>
 where
-    Q: Fetch,
+    Q: Fetch<'a>,
 {
     type Item = Q::Item;
     type IntoIter = Q::Iter;
@@ -52,67 +52,67 @@ impl<'a, T: Component> Fetch<'a> for &T {
     }
 }
 
-pub trait FetchMut<'a> {
-    type Item;
-    type Iter: Iterator<Item = Self::Item>;
-    fn fetch(components: &'a mut ComponentsStorage) -> Self::Iter;
-}
+// pub trait FetchMut<'a> {
+//     type Item;
+//     type Iter: Iterator<Item = Self::Item>;
+//     fn fetch(components: &'a mut ComponentsStorage) -> Self::Iter;
+// }
 
-impl<'a, T: Component> FetchMut<'a> for &mut T {
-    type Item = (&'a Entity, &'a mut T);
-    type Iter = std::collections::hash_map::IterMut<'a, Entity, T>;
+// impl<'a, T: Component> FetchMut<'a> for &mut T {
+//     type Item = (&'a Entity, &'a mut T);
+//     type Iter = std::collections::hash_map::IterMut<'a, Entity, T>;
 
-    fn fetch(components: &'a mut ComponentsStorage) -> Self::Iter {
-        components
-            .get_component_storage_mut::<T>()
-            .expect("Component not found")
-            .iter_mut()
-    }
-}
+//     fn fetch(components: &'a mut ComponentsStorage) -> Self::Iter {
+//         components
+//             .get_component_storage_mut::<T>()
+//             .expect("Component not found")
+//             .iter_mut()
+//     }
+// }
 
-impl<'a, A, B> Fetch<'a> for (&A, &B)
-where
-    A: Component,
-    B: Component,
-{
-    type Item = (Entity, (&'a A, &'a B));
-    type Iter = Zip2<'a, A, B>;
+// impl<'a, A, B> Fetch<'a> for (&A, &B)
+// where
+//     A: Component,
+//     B: Component,
+// {
+//     type Item = (Entity, (&'a A, &'a B));
+//     type Iter = Zip2<'a, A, B>;
 
-    fn fetch(world: &'a ComponentsStorage) -> Self::Iter {
-        let storage_a = world.get_component_storage::<A>().expect("A not found");
-        let storage_b = world.get_component_storage::<B>().expect("B not found");
+//     fn fetch(world: &'a ComponentsStorage) -> Self::Iter {
+//         let storage_a = world.get_component_storage::<A>().expect("A not found");
+//         let storage_b = world.get_component_storage::<B>().expect("B not found");
 
-        // closure to filter and map entities with both components
-        // let filter_map_fn = move |(entity, comp_a): (&Entity, &A)| {
-        //     if let Some(comp_b) = storage_b.get(entity) {
-        //         Some((entity, (comp_a, comp_b)))
-        //     } else {
-        //         None
-        //     }
-        // };
-        // storage_a.iter().filter_map(filter_map_fn)
+//         // closure to filter and map entities with both components
+//         // let filter_map_fn = move |(entity, comp_a): (&Entity, &A)| {
+//         //     if let Some(comp_b) = storage_b.get(entity) {
+//         //         Some((entity, (comp_a, comp_b)))
+//         //     } else {
+//         //         None
+//         //     }
+//         // };
+//         // storage_a.iter().filter_map(filter_map_fn)
 
-        Zip2 {
-            iter_a: storage_a.iter(),
-            storage_b,
-        }
-    }
-}
+//         Zip2 {
+//             iter_a: storage_a.iter(),
+//             storage_b,
+//         }
+//     }
+// }
 
-pub struct Zip2<'a, A, B> {
-    iter_a: std::collections::hash_map::Iter<'a, Entity, A>,
-    storage_b: &'a std::collections::HashMap<Entity, B>,
-}
+// pub struct Zip2<'a, A, B> {
+//     iter_a: std::collections::hash_map::Iter<'a, Entity, A>,
+//     storage_b: &'a std::collections::HashMap<Entity, B>,
+// }
 
-impl<'a, A, B> Iterator for Zip2<'a, A, B> {
-    type Item = (Entity, (&'a A, &'a B));
+// impl<'a, A, B> Iterator for Zip2<'a, A, B> {
+//     type Item = (Entity, (&'a A, &'a B));
 
-    fn next(&mut self) -> Option<Self::Item> {
-        while let Some((entity, comp_a)) = self.iter_a.next() {
-            if let Some(comp_b) = self.storage_b.get(entity) {
-                return Some((*entity, (comp_a, comp_b)));
-            }
-        }
-        None
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         while let Some((entity, comp_a)) = self.iter_a.next() {
+//             if let Some(comp_b) = self.storage_b.get(entity) {
+//                 return Some((*entity, (comp_a, comp_b)));
+//             }
+//         }
+//         None
+//     }
+// }
--- ecs/src/system.rs
+++ ecs/src/system.rs
@@ -9,7 +9,7 @@ pub trait System {
 
 impl<Q> System for dyn Fn(Query<'_, Q>)
 where
-    Q: Fetch,
+    for<'a> Q: Fetch<'a>,
 {
     fn run(&self, components: &mut ComponentsStorage) {
         let query = Query::new(components);
@@ -19,10 +19,39 @@ where
 
 impl<Q> System for fn(Query<'_, Q>)
 where
-    Q: Fetch,
+    for<'a> Q: Fetch<'a>,
 {
     fn run(&self, components: &mut ComponentsStorage) {
         let query = Query::new(components);
+        (self)(query);
+    }
+}
+
+pub struct FnSystem<F>(pub F);
+
+impl<F> System for FnSystem<F>
+where
+    F: IntoSystem,
+{
+    fn run(&self, components: &mut ComponentsStorage) {
+        self.0.run(components);
+    }
+}
+
+pub trait IntoSystem<Q>
+where
+    for<'a> Q: Fetch<'a>,
+{
+    fn run(&self, components: &mut ComponentsStorage);
+}
+
+impl<F, Q> IntoSystem<Q> for F
+where
+    F: for<'a> Fn(Query<'a, Q>),
+    for<'a> Q: Fetch<'a>,
+{
+    fn run(&self, components: &mut ComponentsStorage) {
+        let query = Query::<Q>::new(components);
         (self)(query);
     }
 }
